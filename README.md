1ae1873be9abe7c41f584ac3f272cf8430680155
Ben Constable <ben@benconstable.co.uk>
2013-05-06 21:21:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,120 @@
+# pathbuilderjs
+
+Easily modify parameters in paths.
+
+This simple library allows you to work easily with parameters in routing
+frameworks like [Backbone.js](http://backbonejs.org/) and [pathjs](https://github.com/mtrpcic/pathjs), although it can be used for setting parameters in any
+delimiter-separated string containing parameter-value pairs.
+
+## Installation
+
+In the browser:
+
+```html
+<script src="dist/pathbuilder.min.js"></script> 
+```
+
+**Note:** This library requires `Array.indexOf`, so you'll need [a polyfill](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/IndexOf#Compatibility) for
+browsers without ES5 support.
+
+On the server:
+
+```
+npm install pathbuilder
+```
+
+## Usage
+
+```js
+var pb = new PathBuilder();
+
+var path = pb
+    .prefix('/search/') // Parameters will be parsed after this
+    .path('/search/query/hello+there/categories/hairy/')
+    .param('query', 'hello+there+big+and')
+    .param('filter', 'old')
+    .path();
+
+console.log(path);
+
+// "/search/query/hello+there+big+and/categories/hairy/filter/old/"
+```
+
+Or on the server:
+
+```js
+var pb = require('pathbuilder');
+
+// ...
+```
+
+## API
+
+### .delimiter(delimiter)
+
+Set or get the path delimiter. This is `/` by default.
+
+**Note:** Setting the delimiter will reset the path.
+
+```js
+pb.delimiter("'");
+pb.delimiter(); // "'"
+```
+
+### .ordering(ordering)
+
+Set or get the current custom ordering.
+
+This should be an array of parameter names in the order you'd like them to
+appear in the resulting path.
+
+```js
+pb.ordering(['categories', 'query']);
+pb.ordering(); // ['categories', 'query']
+
+pb.prefix('/search/');
+pb.path('/search/query/hello+there/categories/hairy/');
+pb.path(); // "/search/categories/hairy/query/hello+there/"
+```
+
+### .param(key, value)
+
+Set or get a parameter in the path.
+
+```js
+pb.param('first', 'the-first');
+pb.param('second', 'the-second');
+
+pb.param('first'); // "the-first"
+pb.param('second'); // "the-second"
+
+pb.path(); // "/first/the-first/second/the-second"
+```
+
+### .path(path)
+
+Set or get the current path.
+
+```js
+pb.path('/query/hello+there/categories/hairy/');
+pb.path(); // "/query/hello+there/categories/hairy/"
+```
+
+### .prefix(prefix)
+
+Set or get the path prefix. Anything after the `.prefix()` will be considered
+to be the parameters.
+
+```js
+pb.prefix('/search/');
+pb.prefix(); // "/search/"
+```
+
+### .toObject()
+
+Get the current path as an object.
+
+```js
+pb.path('/query/hello+there/categories/hairy/');
+pb.toObject(); // { "query": "hello+there", "categories": "hairy" }
+```
---
+++ pathbuilder.js
@@ -0,0 +1,224 @@
+(function () {
+
+
+
+
+/*
+ * PathBuilder.js
+ *
+ * Easily modify parameters in paths.
+ *
+ * This is useful when using routing frameworks like Backbone.js or Path.js.
+ *
+ * See [readme url] for documentation.
+ */
+function PathBuilder() {
+
+
+
+
+    // Store a reference to this for inner functions
+    var that = this;
+
+
+
+
+    var _prefix = '',
+        _path = [],
+        _delimiter = '/',
+        _delimiterLength = _delimiter.length,
+        _ordering = [],
+        _hasLeadingDelimiter = false,
+        _hasTrailingDelimiter = false,
+        _cachedObject;
+
+
+
+
+    /*
+     * Set or get the current delimeter.
+     *
+     * The path is reset if the delimiter is set.
+     */
+    this.delimiter = function (delimiter) {
+        if (!arguments.length) return _delimiter;
+        _delimiter = delimiter;
+        _delimiterLength = delimiter.length;
+        _path = [];
+        _cachedObject = undefined;
+        return this;
+    };
+
+
+
+
+    /*
+     * Set or get the ordering array.
+     */
+    this.ordering = function (ordering) {
+        if (!arguments.length) return _ordering;
+        _ordering = ordering;
+        return this;
+    };
+
+
+
+
+    /*
+     * Set or get a parameter.
+     */
+    this.param = function (key, value) {
+        if (arguments.length === 1) {
+            return that.toObject()[key];
+        } else {
+            var indices = [],
+                start = 0,
+                index;
+
+            while ((index = _path.indexOf(key, start)) > -1) {
+                indices.push(index);
+                start = index + 1;
+            }
+
+            if (indices.length) {
+                for (var i = 0; i < indices.length; i++) {
+                    if ((indices[i] % 2) === 0) {
+                        _path.splice(indices[i] + 1, 1, value);
+                    }
+                }
+            } else {
+                _path.push(key, value);
+            }
+
+            _cachedObject = undefined;
+
+            return that;
+        }
+    };
+
+
+
+
+    /*
+     * Set or get the path.
+     */
+    this.path = function (path) {
+        return !arguments.length ? _getPath() : _setPath(path);
+    };
+
+
+
+
+    /*
+     * Set or get the prefix string.
+     */
+    this.prefix = function (prefix) {
+        if (!arguments.length) return _prefix;
+        _prefix = prefix;
+        return this;
+    };
+
+
+
+
+    /*
+     * Get the current path as an object.
+     */
+    this.toObject = function () {
+        if (typeof _cachedObject === 'undefined') {
+            var obj = {};
+
+            for (var i = 0; i < _path.length; i += 2) {
+                obj[_path[i]] = _path[i + 1];
+            }
+
+            _cachedObject = obj;
+        }
+
+        return _cachedObject;
+    };
+
+
+
+
+    /*
+     * Get the path.
+     */
+    function _getPath() {
+        var object = that.toObject(),
+            p = _hasLeadingDelimiter ? _delimiter : '';
+
+        // Add explicitly-ordered params first
+        for (var i = 0; i < _ordering.length; i++) {
+            var key = _ordering[i];
+            if ((typeof object[key] !== 'undefined') && object[key] !== '') {
+                p += key + _delimiter + object[key] + _delimiter;
+                delete object[key];
+            }
+        }
+
+        // Add the rest of the params
+        for (var jey in object) {
+            if (object[jey] !== '') {
+                p += jey + _delimiter + object[jey] + _delimiter;
+            }
+        }
+
+        if (!_hasTrailingDelimiter) {
+            p = p.substring(0, p.length - _delimiterLength);
+        }
+
+        return _prefix + p;
+    }
+
+
+
+
+    /*
+     * Set the path.
+     */
+    function _setPath(path) {
+        if (path.indexOf(_prefix) === 0) {
+            path = path.substring(_prefix.length);
+        }
+
+        _hasLeadingDelimiter =
+            path.substring(0, _delimiterLength) === _delimiter,
+        _hasTrailingDelimiter = 
+            path.substring(path.length - _delimiterLength) === _delimiter;
+        
+        if (_hasLeadingDelimiter) {
+            path = path.substring(_delimiterLength, path.length);
+        }
+
+        if (_hasTrailingDelimiter) {
+            path = path.substring(0, path.length - _delimiterLength);
+        }
+
+        _path = path.split(_delimiter);
+        _cachedObject = undefined;
+
+        return that;
+    }
+}
+
+
+
+
+PathBuilder.VERSION = '0.1.0';
+
+
+
+
+// Export for Node and the browser
+var root = this;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = new PathBuilder();
+} else {
+    root.PathBuilder = PathBuilder;
+}
+
+
+
+
+}).call(this);
